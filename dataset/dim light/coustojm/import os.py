7fb3d72f657817fc9d146aaca692bebe967f5959
M.Abdul Rehman <100429268+Programmingwithab1@users.noreply.github.com>
2024-09-26 11:08:11 +0500

    DataSet + Implementation

---
+++ dataset/dim light/coustojm/import os.py
@@ -0,0 +1,20 @@
+import os
+from PIL import Image
+
+def convert_jpeg_to_jpg(input_dir, output_dir):
+    """
+    Converts all JPEG images in the input directory to JPG format and saves them in the output directory.
+    """
+    for filename in os.listdir(input_dir):
+        if filename.lower().endswith(".jpeg") or filename.lower().endswith(".jpg"):
+            img_path = os.path.join(input_dir, filename)
+            img = Image.open(img_path)
+            new_filename = os.path.splitext(filename)[0] + ".jpg"
+            output_path = os.path.join(output_dir, new_filename)
+            img.save(output_path, "JPEG")
+
+if __name__ == "__main__":
+    input_directory = r"C:\Users\mabdu\Downloads\dim light\coustojm"
+    output_directory = r"C:\Users\mabdu\Downloads\dim light\coustojm\jpg files"  # Replace with your desired output directory
+    convert_jpeg_to_jpg(input_directory, output_directory)
+    print("Conversion completed successfully!")
